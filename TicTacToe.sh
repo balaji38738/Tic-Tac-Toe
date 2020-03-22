08248c1deeb08abbc95715966a34548cb90ac479
balaji38738 <bijjapwar123@gmail.com>
2020-03-23 04:35:57 +0530

    Board reset function added

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -5,3 +5,18 @@
 #Date:- 22 March 2020
 
 echo "-------Tic Tac Toe-------"
+declare -A matrix
+
+function resetBoard() {
+	echo "New game starts"
+	filledCells=0
+	for (( i=0; i<3; i++ ))
+	do
+		for (( j=0; j<3; j++ ))
+		do
+			matrix[$i,$j]=" "
+		done
+	done
+}
+
+resetBoard
