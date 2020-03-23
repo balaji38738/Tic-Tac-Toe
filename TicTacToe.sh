6f285d10facfe6b8d8da0b08c827c27fa1e8b336
balaji38738 <bijjapwar123@gmail.com>
2020-03-23 19:02:48 +0530

    ADDED: functions to check winning possibility of computer

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -13,6 +13,7 @@ USER=0
 COMP=1
 TRUE=1
 FALSE=0
+
 function resetBoard() {
 	echo -e "\nNew game starts"
 	filledCells=0
@@ -43,6 +44,7 @@ function displayBoard() {
 	done
 }
 
+#Checks equality of three cell values
 function areThreeEqual() {
    cell1=$1
    cell2=$2
@@ -62,6 +64,7 @@ function areThreeEqual() {
    fi
 }
 
+#Finds if anyone won by checking if any row, column or diagonal have same values
 function checkIfGameWon() {
    isWon=$FALSE
    for (( i=0; i<3; i++ ))
@@ -90,6 +93,35 @@ function checkIfGameWon() {
    fi
 }
 
+#Checks if any two cell values are equal
+function anyTwoEqual() {
+	cell1=$1
+	cell2=$2
+	cell3=$3
+	if [[ "$cell1" = "0" && "$cell2" = "0" ]]
+	then
+		canCompWin=$TRUE
+	elif [[ "$cell2" = "0" && "$cell3" = "0" ]]
+	then
+		canCompWin=$TRUE
+	elif [[ "$cell1" = "0" && "$cell3" = "0" ]]
+	then
+		canCompWin=$TRUE
+	fi
+}
+
+#Checks for winnnig computer winning possibilities
+function checkingCompWinning() {
+	canCompWin=$FALSE
+	for (( i=0; i<3; i++ ))
+	do
+		anyTwoEqual ${matrix[$i,0]} ${matrix[$i,1]} ${matrix[$i,2]}
+		anyTwoEqual ${matrix[0,$i]} ${matrix[1,$i]} ${matrix[2,$i]}
+	done
+	anyTwoEqual ${matrix[0,0]} ${matrix[1,1]} ${matrix[2,2]}
+	anyTwoEqual ${matrix[0,2]} ${matrix[1,1]} ${matrix[2,0]}
+}
+
 
 resetBoard
 
@@ -107,3 +139,12 @@ displayBoard
 checkIfGameWon
 
 echo -e "\nThis is smart computer"
+
+checkingCompWinning
+
+if [ $canCompWin -eq $TRUE ]
+then
+   echo "Computer can win now"
+else
+	echo "Computer can't win now"
+fi
