86b256922c83e192f5b39323b0990ab06a5a8333
balaji38738 <bijjapwar123@gmail.com>
2020-03-23 13:14:02 +0530

    ADDED: functions to check if somebody won

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -42,6 +42,54 @@ function displayBoard() {
 	done
 }
 
+function areThreeEqual() {
+   cell1=$1
+   cell2=$2
+   cell3=$3
+   if [ "$cell1" != "" ]
+   then
+      if [[ $cell1 = $cell2 && $cell2 = $cell3 ]]
+      then
+         isWon=$TRUE
+         if [ "$cell1" = "$userChar" ]
+         then
+            winner=$userChar
+         else
+            winner=$compChar
+         fi
+      fi
+   fi
+}
+
+function checkIfGameWon() {
+   isWon=$FALSE
+   for (( i=0; i<3; i++ ))
+   do
+      areThreeEqual ${matrix[$i,0]} ${matrix[$i,1]} ${matrix[$i,2]}
+      areThreeEqual ${matrix[0,$i]} ${matrix[1,$i]} ${matrix[2,$i]}
+   done
+   areThreeEqual ${matrix[0,0]} ${matrix[1,1]} ${matrix[2,2]}
+   areThreeEqual ${matrix[0,2]} ${matrix[1,1]} ${matrix[2,0]}
+   if [ $isWon -eq $TRUE ]
+   then
+      if [ "$winner" = $userChar ]
+      then
+         echo "You Won"
+      elif [ "$winner" = $compChar ]
+      then
+         echo "Computer Won"
+      fi
+   else
+      if [ $filledCells -eq 9 ]
+      then
+         echo "Game Tie"
+      else
+         turn=$((1-turn))
+      fi
+   fi
+}
+
+
 resetBoard
 
 echo "You are assigned '$userChar'"
